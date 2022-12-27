f618999507c4854d6f7971653c0c9d54e182b905
Daredemodaisuki <64051837+Daredemodaisuki@users.noreply.github.com>
2022-12-27 18:45:22 +0800

    Add files via upload

---
+++ Lesson7/题目1_1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Teacher //教师类
+{
+protected:
+    string name;
+    int age;
+    string sex;
+    string addr;
+    string tel;
+    string title;
+public:
+    Teacher(string t_name, int t_age, string t_sex, string t_addr, string t_tel, string t_title)
+        :name(t_name), age(t_age), sex(t_sex), addr(t_addr), tel(t_tel), title(t_title) {};
+};
+
+class Cadre //干部类
+{
+protected:
+    string name;
+    int age;
+    string sex;
+    string addr;
+    string tel;
+    string post;
+public:
+    Cadre(string c_name, int c_age, string c_sex, string c_addr, string c_tel, string c_post)
+        :name(c_name), age(c_age), sex(c_sex), addr(c_addr), tel(c_tel), post(c_post) {};
+};
+
+class Teacher_Cadre :public Teacher, public Cadre //教师兼干部类
+{
+private:
+    int wages;
+public:
+    Teacher_Cadre(string tc_name = "Unnamed", int tc_age = 0, string tc_sex = "Other", string tc_addr = "Shimokitazawa", string tc_tel = "114514", string tc_title = "Untitled", string tc_post = "Undifined", int tc_wages = 0)
+        :Teacher(tc_name, tc_age, tc_sex, tc_addr, tc_tel, tc_title), Cadre(tc_name, tc_age, tc_sex, tc_addr, tc_tel, tc_post), wages(tc_wages) {};
+    void putin();
+    void display();
+};
+
+void Teacher_Cadre::display()
+{
+    cout << Teacher::name << " " << Teacher::age << " " << Teacher::sex << " " << Teacher::title << " " << Teacher::addr << " " << Teacher::tel << " " << Cadre::post << " " << wages;
+}
+
+void Teacher_Cadre::putin()
+{
+    cin >> Teacher::name >> Teacher::age >> Teacher::sex >> Teacher::title >> Teacher::addr >> Teacher::tel >> Cadre::post >> wages;
+    Cadre::age = Teacher::age;
+    Cadre::sex = Teacher::sex;
+    Cadre::addr = Teacher::addr;
+    Cadre::tel = Teacher::tel;
+}
+
+int main()
+{
+    Teacher_Cadre person;
+    person.putin();
+    person.display();
+    return 0;
+}
---
+++ Lesson7/题目1_2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Person //人类
+{
+protected:
+    string name;
+    int age;
+    string sex;
+    string addr;
+    string tel;
+public:
+    Person(string p_name, int p_age, string p_sex, string p_addr, string p_tel)
+        :name(p_name), age(p_age), sex(p_sex), addr(p_addr), tel(p_tel) {};
+};
+
+class Teacher :virtual public Person //教师类
+{
+protected:
+    string title;
+public:
+    Teacher(string t_name, int t_age, string t_sex, string t_addr, string t_tel, string t_title)
+        :Person(t_name, t_age, t_sex, t_addr, t_tel), title(t_title) {};
+};
+
+class Cadre :virtual public Person //干部类
+{
+protected:
+    string post;
+public:
+    Cadre(string c_name, int c_age, string c_sex, string c_addr, string c_tel, string c_post)
+        :Person(c_name, c_age, c_sex, c_addr, c_tel), post(c_post) {};
+};
+
+class Teacher_Cadre :public Teacher, public Cadre //教师兼干部类
+{
+private:
+    int wages;
+public:
+    Teacher_Cadre(string tc_name = "Unnamed", int tc_age = 0, string tc_sex = "Other", string tc_addr = "Shimokitazawa", string tc_tel = "114514", string tc_title = "Untitled", string tc_post = "Undifined", int tc_wages = 0)
+        :Person(tc_name, tc_age, tc_sex, tc_addr, tc_tel), Teacher(tc_name, tc_age, tc_sex, tc_addr, tc_tel, tc_title), Cadre(tc_name, tc_age, tc_sex, tc_addr, tc_tel, tc_post), wages(tc_wages) {};
+    void putin();
+    void display();
+};
+
+void Teacher_Cadre::display()
+{
+    cout << name << " " << age << " " << sex << " " << title << " " << addr << " " << tel << " " << post << " " << wages;
+}
+
+void Teacher_Cadre::putin()
+{
+    cin >> name >> age >> sex >> title >> addr >> tel >> post >> wages;
+}
+
+int main()
+{
+    Teacher_Cadre person;
+    person.putin();
+    person.display();
+    return 0;
+}
